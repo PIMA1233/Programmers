0911fa056bd1459ee552f3507eaa521a6395558b
CodingFOX <82526468+PIMA1233@users.noreply.github.com>
2023-03-23 18:26:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    
+    for(answer=1;answer<n;answer++){
+        if(a%2 == 1) a++;
+        if(b%2 == 1) b++;
+        a/=2;
+        b/=2;
+        
+        if(a==b){
+            break;
+        }
+    }
+
+    return answer;
+}
