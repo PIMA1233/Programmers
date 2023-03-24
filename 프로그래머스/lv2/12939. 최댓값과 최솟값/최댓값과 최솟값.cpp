b5d19845a3bbd7f867116a73b1c32e441abec79a
CodingFOX <82526468+PIMA1233@users.noreply.github.com>
2023-03-24 21:20:02 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,25 +1,27 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
-    string num="";
-    vector<int> arr;
+    string answer = ""; // 정답 string
+    int max_s = -2134567890, min_s = 2134567890; // 최대, 최소값 int
+    
+    string temp = ""; // 찾아낸 숫자 저장
+    // s에서 숫자 찾아내 최대, 최소값 저장하는 for문
     for(int i=0;i<s.length();i++){
-        if(s[i]!=' '){
-            num+=s[i];
-        } else{
-            arr.push_back(stoi(num));
-            num.clear();
+        if(s[i] == ' '){
+            if(max_s < stoi(temp)) max_s = stoi(temp);
+            if(min_s > stoi(temp)) min_s = stoi(temp);
+            temp.clear();
+        } else {
+            temp += s[i];
         }
     }
-    arr.push_back(stoi(num));
+    if(max_s < stoi(temp)) max_s = stoi(temp);
+    if(min_s > stoi(temp)) min_s = stoi(temp);
     
-    sort(arr.begin(),arr.end());
-    answer += to_string(arr.front()) + " " + to_string(arr.back());
+    answer = to_string(min_s) + " " + to_string(max_s);
     
     return answer;
 }
