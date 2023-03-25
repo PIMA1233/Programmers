6ada37429ec66e0e7927054738bbf452de04afd3
CodingFOX <82526468+PIMA1233@users.noreply.github.com>
2023-03-25 19:58:18 +0900

    [level 2] Title: 위장, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> styles;
+    
+    for(auto cloth : clothes){
+        styles[cloth[1]]+=1;
+    }
+    
+    for(auto it = styles.begin();it != styles.end(); it++){
+        answer *= it->second+1;
+    }
+    
+    return answer-1;
+}
